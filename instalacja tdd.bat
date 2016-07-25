4af9310ad0476856bad0a6025f4bf4acf2f253e6
kes <born-kes@o2.pl>
2016-07-26 01:06:07 +0200

    framework Jasmine - do testów jednostkowych silnik do PhantomJS do testów na przeglądarkach WebKit +biblioteki

--- instalacja tdd.bat
+++ instalacja tdd.bat
@@ -4,3 +4,9 @@
 // Środowisko do uruchamiania testów
 npm install karma --save-dev
 npm install grunt-karma --save-dev
+npm install karma-cli --save-dev
+
+// Jasmine : framework służący do pisania testów jednostkowych
+npm install karma-jasmine --save-dev
+// silnik dla testów na PhantomJS (przeglądarka na silniku WebKit)
+npm install karma-phantomjs-launcher --save-dev
