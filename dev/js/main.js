44b92491654745b81988018dbaec3ee8210bc9a5
kes <born-kes@o2.pl>
2016-07-26 18:17:47 +0200

    Pliki frontu do testów

---
+++ dev/js/main.js
@@ -0,0 +1,12 @@
+var counter = {add : function(num1, num2) {
+        alert(num1+'+'+  num2);
+        return num1 + num2;
+    },
+    subtract : function(num1, num2) {
+        alert(num1+'-'+ num2);
+        return num1 - num2;
+    }
+};
+
+
+counter.add(1, 3);
